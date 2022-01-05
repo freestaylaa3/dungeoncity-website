d4f2b3b71f8cf34fe0c9d810e513237188796ff0
Furkan Okuk <furkanokuk7@hotmail.com>
2022-01-05 15:57:37 +0300

    .

--- src/app/main/admin/panel-item/panel-item.component.ts
+++ src/app/main/admin/panel-item/panel-item.component.ts
@@ -81,6 +81,28 @@ export class PanelItemComponent implements OnInit {
     }
   }
 
+  search() {
+    // Declare variables
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("itemTable");
+    tr = table.getElementsByTagName("tr");
+  
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[0];
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }
+    }
+  }
+
   loadEditItem(id: number) {
     this.itemService.getItemById(id).subscribe((data) => {
       this.editItemForm.setValue({
